c74d168645fcb3f470b11057c7fd4cbba23e296d
kartikeyamishra <kartikeyamishra7541@gmail.com>
2026-09-18 21:36:05 +0530

    modified the file

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -9,14 +9,10 @@ public class HelloWorld {
 
 	System.out.println ("Hello There");// main branch+ //Adding line from b1
 		
-<<<<<<< HEAD
+
 System.out.println ("Hello There");//new from main 
-		//Dev 1 content
-	
-=======
-System.out.println ("Hello There");//new from main
-    //b1 content
->>>>>>> 8809464 (b1 dev added)
+		//Dev 1 content + //b1 content
+
 
 
 
